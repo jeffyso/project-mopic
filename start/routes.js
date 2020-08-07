aeb2816d9a853c960f1b0133187a0d9fe76970ba
Narisza_k <62646342+Cruzel28@users.noreply.github.com>
2020-08-07 15:52:02 +0700

    routes update

--- start/routes.js
+++ start/routes.js
@@ -20,8 +20,14 @@ const Route = use('Route')
 
 Route.on('/').render('welcome')
 
+Route.get("/home","AuthController.home");
 
 Route.get("/login","AuthController.login");
 Route.post("/login","AuthController.loginUser");
 
 Route.get("/register","AuthController.register");
+Route.post("/register","AuthController.registerUser");
+
+Route.get("/news","AuthController.news");
+
+Route.get("/movies","AuthController.movies");
