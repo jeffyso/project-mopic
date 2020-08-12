730b7e3f628e193f91c0be292efe78ca85ec084b
jeffyso <jeffyzaja@gmail.com>
2020-08-12 20:23:29 +0700

    update-rating-endgame&1917

--- app/Controllers/Http/AuthController.js
+++ app/Controllers/Http/AuthController.js
@@ -5,7 +5,7 @@ let token;
 let user ;
 //--------------------------------------------------------EndGame Variable---------------------------------------------------------------------
 
-let rating;
+let rating=0;
 let total=0;
 let sumUse = {user5:0,user4:0,user3:0,user2:0,user1:0}
 let sumEnd ={sum1:0,sum2:0,sum3:0,sum4:0,sum5:0};
@@ -14,10 +14,10 @@ let end = {star1:0,star2:0,star3:0,star4:0,star5:0}
 //--------------------------------------------------------1917 Variable---------------------------------------------------------------------
 
 
-let rating19;
+let ratingone=0;
 let total19=0;
 let sumUse19 = {user5:0,user4:0,user3:0,user2:0,user1:0}
-let sum19 ={sum1:0,sum2:0,sum3:0,sum4:0,sum5:0};
+let sum19 ={s1:0,s2:0,s3:0,s4:0,s5:0};
 let onenine = {star1:0,star2:0,star3:0,star4:0,star5:0}
 
 
@@ -85,6 +85,7 @@ class AuthController {
 
 //--------------------------------------------------------endgame---------------------------------------------------------------------
     endgame({view}){
+        console.log(rating)
         return view.render("endgame",{token,user,rating});
     }
     
@@ -145,13 +146,14 @@ class AuthController {
             sumRate = sumEnd.sum1+sumEnd.sum2+sumEnd.sum3+sumEnd.sum4+sumEnd.sum5;
             sumRate = sumRate/total;
 
-            console.log("total: "+sumRate);
+            rating = sumRate;
+
+            console.log("total: "+rating);
 
 
-        return view.render("/endgame")
+        return view.render("/endgame", { rating })
     }
 
-    
 //--------------------------------------------------------joker---------------------------------------------------------------------
     joker({view}){
         return view.render("joker",{token,user});
@@ -159,8 +161,73 @@ class AuthController {
 
     
 //--------------------------------------------------------1917---------------------------------------------------------------------
-    1917({view}){
+    // ตรงนี้ตั้งชื่ออย่างงี้ไม่ได้นะ
+
+   one({view}){
         return view.render("1917",{token,user,rating19});
+    }
+
+
+    oneRate({view,request,response}){
+
+        const {one} = request.body
+        // sumrate19 = 0;
+        if(one == 5){
+            total19 = total19 + 1;
+            onenine.star5 = one;
+            sumUse19.user5 = sumUse19.user5 + 1;
+            onenine.star5 = (onenine.star5*sumUse19.user5)
+            sum19.s5 = sum19.s5+onenine.star5;
+            
+            // console.log("star :"+sum19.s5);
+            // console.log("user :"+total19);
+        }
+
+        else if(one == 4){
+            total19 = total19 + 1;
+            onenine.star4 = one; //4
+            sumUse19.user4 = sumUse19.user4 + 1;
+            onenine.star4 = (onenine.star4*sumUse19.user4)
+            sum19.s4 = sum19.s4+onenine.star4;
+            
+            
+        }
+
+        else if(one == 3){
+            total19 = total19 + 1;
+            onenine.star3 = one;
+            sumUse19.user3 = sumUse19.user3 + 1;
+            onenine.star3 = (onenine.star3*sumUse19.user3)
+            sum19.s3 = sum19.s3+onenine.star5;
+            
+            
+        }
+
+        else if(one == 2){
+            total19 = total19 + 1;
+            onenine.star2 = one;
+            sumUse19.user2 = sumUse19.user2 + 1;
+            onenine.star2 = (onenine.star2*sumUse19.user2)
+            sum19.s2 = sum19.s2+onenine.star2;
+            
+            
+        }
+        else if(one == 1){
+            total19 = total19 + 1;
+            onenine.star1 = one;
+            sumUse19.user1 = sumUse19.user1 + 1;
+            onenine.star1 = (onenine.star1*sumUse19.user1)
+            sum19.s1 = sum19.s1+onenine.star1;
+            
+            
+        }
+
+        ratingone = sum19.s1+sum19.s2+sum19.s3+sum19.s4+sum19.s5;
+        ratingone = ratingone/total19;
+
+            
+
+        return view.render("/1917",{ratingone})
     }
 
 //--------------------------------------------------------parasite---------------------------------------------------------------------
